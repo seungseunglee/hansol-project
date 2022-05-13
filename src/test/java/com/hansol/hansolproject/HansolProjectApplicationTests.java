ca1e6f6c3b691438b59dd5c331ac39a64aa8c716
이승현 <dltmdgus741@gmail.com>
2022-05-13 14:11:23 +0900

    Initial commit

---
+++ src/test/java/com/hansol/hansolproject/HansolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hansol.hansolproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HansolProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
